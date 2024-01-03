7761cdbad10b261ea898b0a697df3d5522464165
jay9949 <jay37192@gmail.com>
2024-01-03 12:41:59 +0530

    #

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+import { Fragment } from "react";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  // fetch data from API
+  const client = await MongoClient.connect(
+    "mongodb+srv://jaysuthar:Jaysuthar9949@cluster0.zrufcjj.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
